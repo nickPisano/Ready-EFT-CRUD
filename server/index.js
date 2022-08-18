047d04e03c1352c9d8daa7ae718d9aa95b99f312
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-17 19:08:50 -0700

    Changed .gitignore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const mySQL = require("mysql")
+
+const db = mySQL.createConnectione({
+    user: "root",
+    host: "localhost",
+    password: "abc123",
+    database: "readyEft_DB"
+});
+
+app.post("/create", (req, res) => {
+    const name = req.body
+})
 
 app.listen(3001, () => {
     console.log("Server is Running on 3001")
-})
+});
