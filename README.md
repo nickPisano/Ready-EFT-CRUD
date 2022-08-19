2f3979657be4430b14daa211010f80fd30d55f3f
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-19 06:00:20 -0700

    Cleaned up server.js and Made simple README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ready-EFT-CRUD
+## How To Intall and Use this program
+1. First Clone the repository
+2. npm install in the root folder within terminal
+3. npm run develop in the root folder within terminal
--- server/server.js
+++ server/server.js
@@ -1,26 +1,13 @@
 const express = require('express');
-// const mySQL = require("mysql");
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-
-// const db = mySQL.createConnection({
-//     user: "root",
-//     host: "localhost",
-//     password: "abc123",
-//     database: "readyEft_DB"
-// });
-
 // app.post("/create", (req, res) => {
 //     const name = req.body
 // })
-
-// app.listen(3001, () => {
-//     console.log("Server is Running on 3001")
-// });
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
