ab6267ca1aa04be0086b7df1090bb3b6a7598dc7
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-19 05:33:50 -0700

    Got Backend and frontend to work

--- server/server.js
+++ server/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
+// const mySQL = require("mysql");
+const sequelize = require('./config/connection');
 const app = express();
-const mySQL = require("mysql")
+const PORT = process.env.PORT || 3001;
 
-const db = mySQL.createConnectione({
-    user: "root",
-    host: "localhost",
-    password: "abc123",
-    database: "readyEft_DB"
-});
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.post("/create", (req, res) => {
-    const name = req.body
-})
+// const db = mySQL.createConnection({
+//     user: "root",
+//     host: "localhost",
+//     password: "abc123",
+//     database: "readyEft_DB"
+// });
 
-app.listen(3001, () => {
-    console.log("Server is Running on 3001")
-});
+// app.post("/create", (req, res) => {
+//     const name = req.body
+// })
+
+// app.listen(3001, () => {
+//     console.log("Server is Running on 3001")
+// });
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+  });
