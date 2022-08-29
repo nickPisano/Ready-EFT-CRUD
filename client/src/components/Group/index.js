f8e4090d1f4221c17f746d746ced1410232f5fe6
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-29 15:25:02 -0700

    Added Message Board

--- client/src/components/Group/index.js
+++ client/src/components/Group/index.js
@@ -7,31 +7,52 @@ socket.on("connect", () => {
   console.log(`You did it, You are connected with id: ${socket.id}`);
 });
 
+
 const Group = () => {
-  // Room State
-  const [room, setRoom] = useState("");
-
-  // Sending Messages to Other users Realtime
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceive] = useState("");
-
-  const joinRoom = () => {
+    // Room State
+    const [room, setRoom] = useState("");
+    
+    // Sending Messages to Other users Realtime
+    const [message, setMessage] = useState("");
+    const [messageReceived, setMessageReceive] = useState("");
+    
+    // checks if user is connected
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    
+    const joinRoom = () => {
     if (room !== "") {
-      socket.emit("join_room", room);
+        socket.emit("join_room", room);
     }
-  };
+};
+
+  socket.on("connect", () => {
+      setIsConnected(true);
+    });
+    
+  socket.on("disconnect", () => {
+    setIsConnected(false);
+  });
   
   const sendMessage = () => {
     socket.emit("send_message", { message, room });
   };
-  
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceive(data.message);
-    });
-  }, [socket]);
 
-  return (
+  useEffect(() => {
+      socket.on("receive_message", (data) => {
+        setMessageReceive(data.message);
+        displayMessage(data.message)
+    });
+}, [socket]);
+
+const displayMessage = (data) => {
+    console.log("display message function called");
+    console.log(data);
+    const div = document.createElement("div")
+    div.textContent = data;
+    document.getElementById("message-container").append(div)
+}
+
+return (
     <div className="chatRoom">
       <h2>Your connected with id: {socket.id}</h2>
       <div>
@@ -40,7 +61,7 @@ const Group = () => {
           onChange={(event) => {
             setRoom(event.target.value);
           }}
-        />
+          />
         <button onClick={joinRoom}> Join Room</button>
         <div className="messageDiv">
           <input
@@ -48,11 +69,12 @@ const Group = () => {
             onChange={(event) => {
               setMessage(event.target.value);
             }}
-          />
+            />
           <button onClick={sendMessage}>Send Message</button>
         </div>
       </div>
-      <h1>Message: {messageReceived}</h1>
+      <div id="message-container"></div>
+      <p>Connected: {"" + isConnected}</p>
     </div>
   );
 };
