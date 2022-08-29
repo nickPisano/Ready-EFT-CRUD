c2b942546fb5c4ec5e36f588363b56bf75b002d8
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-29 07:25:47 -0700

    Added React Routes to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
+import Header from "./components/Header";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <header>
-        <h1>Ready-EFT</h1>
-      </header>
+      <Router>
       <div>
-        <iframe 
-        src="https://www.youtube.com/embed/gEbJjN6rtQE?autoplay=1&amp;loop=1&amp;enablejsapi=1&amp;&amp;playerapiid=featuredytplayer&amp;controls=0&amp;modestbranding=1&amp;rel=0&amp;showinfo=0&amp;color=white&amp;iv_load_policy=3&amp;theme=light&amp;wmode=transparent&amp;mute=1"
-        frameborder="0"
-        width="1920"
-        height="1080"
-        className="ytplayer"
-        allow="fullscreen"
-        title="Escape from Tarkov ARENA"></iframe>
-        </div>
+        <Header />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      </Router>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
