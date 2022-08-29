388e737da2a435e2affe505c7a139c05bb340e1b
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-29 12:20:32 -0700

    Added Chatroom

--- client/src/components/Group/index.js
+++ client/src/components/Group/index.js
@@ -1,16 +1,38 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080")
+const socket = io.connect("http://localhost:8080");
 
 socket.on("connect", () => {
-    console.log(`You did it, You are connected with id: ${socket.id}`)
-})
+  console.log(`You did it, You are connected with id: ${socket.id}`);
+});
 
 const Group = () => {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceive] = useState("")
+  const sendMessage = () => {
+    socket.emit('send_message', { message } )
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceive(data.message)
+    })
+  }, [socket])
+
   return (
     <div>
-        <h1>Your connected with id: {socket.id}</h1>
+      <h1>Your connected with id: {socket.id}</h1>
+      <div>
+        <div className="messageDiv">
+          <input placeholder="Message" 
+          onChange={(event) => {
+            setMessage(event.target.value)
+          }}/>
+          <button onClick={sendMessage}>Send Message</button>
+        </div>
+      </div>
+      <h1>Message: {messageReceived}</h1>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
 const express = require("express");
+const app = express();
+const http = require('http');
 const sequelize = require("./config/connection");
 const PORT = process.env.PORT || 3001;
+const cors = require("cors")
+app.use(cors())
 
-const io = require('socket.io')(8080,  {
+
+const io = require("socket.io")(8080, {
   cors: {
-      origin: ['http://localhost:3000']
-}
-})
-const app = express();
+    origin: ["http://localhost:3000"],
+  },
+});
 
-// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-io.on('connection', socket => {
-  console.log(socket.id)
-})
+io.on("connection", socket => {
+  console.log(socket.id);
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
+});
 
 // app.post("/create", (req, res) => {
 //     const name = req.body
