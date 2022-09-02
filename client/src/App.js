7a29e6d94575a7680743ddd207101daef1bce4ee
Nick Pisano <pisano.nicholas@yahoo.com>
2022-09-02 00:13:00 -0700

    Made Chatroom A Component on the main page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Group from "./components/Group";
+import Group from "./pages/Group";
 
 function App() {
   return (
---
+++ client/src/pages/Group.js
@@ -0,0 +1,12 @@
+import Chatroom from "../components/Chatroom";
+import React from "react";
+
+const Group = () => {
+  return (
+    <div>
+        <Chatroom />
+    </div>
+  );
+};
+
+export default Group;
